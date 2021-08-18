9ec4a6c68f35532e933694e77efc00f2f4f52abf
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2021-08-18 19:30:42 +0300

    Add Exercise 10.10: a platform specific font

--- src/theme.js
+++ src/theme.js
@@ -1,46 +1,54 @@
+import { Platform } from 'react-native';
+
+const platformFont =  Platform.select({
+  android: 'Roboto',
+  ios: 'Arial',
+  default: 'System',
+});
+
 const theme = {
-    colors: {
-      mainBackGround: '#e1e4e8',
-      textPrimary: '#24292e',
-      textSecondary: '#586069',
-      primary: '#0366d6',
-      appBarBackground: '#24292e',
-      white: 'white',
-      error: '#d73a4a'
-    },
-    fontSizes: {
-      body: 14,
-      subheading: 16,
-      heading: 20
-    },
-    fonts: {
-      main: 'System',
-    },
-    fontWeights: {
-      normal: '400',
-      bold: '700',
-    },
-    padding: {
-      basic: 20,
-      small: 10
-    },
-    logo: {
-      width: 60,
-      height: 60,
-    },
-    button: {
-      height: 40,
-      width: 300,
-      paddingHorizontal: 5,
-    },
-    margins: {
-      marginXs: 5
-    },
-    border: {
-      borderRadius: 4,
-      borderWidth: 1,
-      borderStyle: 'solid'
-    }
-  };
-  
-  export default theme;
+  colors: {
+    mainBackGround: '#e1e4e8',
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    appBarBackground: '#24292e',
+    white: 'white',
+    error: '#d73a4a'
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    heading: 20
+  },
+  fonts: {
+    main: platformFont
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+  padding: {
+    basic: 20,
+    small: 10
+  },
+  logo: {
+    width: 60,
+    height: 60,
+  },
+  button: {
+    height: 40,
+    width: 300,
+    paddingHorizontal: 5,
+  },
+  margins: {
+    marginXs: 5
+  },
+  border: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid'
+  }
+};
+
+export default theme;
