bcc431d5dc9fa99102e056eb476335dec20c92f0
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2021-08-19 19:55:46 +0300

    Refactor repository fetching to own hook, install apollo and graphql

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -15,22 +16,8 @@ const renderItem = ({ item }) => (
 );
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
-
-  const fetchRepositories = async () => {
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.10.44:5000/api/repositories');
-    const json = await response.json();
-
-    console.log(json);
-
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
+  const { repositories } = useRepositories();
+  
   // Get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
