310a8af45b36a790e73480c504230e181c112c92
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2021-12-30 21:54:39 +0200

    Show reviews for a repository

--- src/components/SingleRepositoryItem.jsx
+++ src/components/SingleRepositoryItem.jsx
@@ -4,6 +4,7 @@ import theme from '../theme';
 import useRepository from '../hooks/useRepository';
 import RepositoryContent from './RepositoryContent';
 import Text from './Text';
+import { format } from 'date-fns';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,19 +12,28 @@ const styles = StyleSheet.create({
     paddingTop: theme.padding.basic,
     paddingLeft: theme.padding.basic,
     backgroundColor: theme.colors.white
-  }
+  },
+  reviewItem: {
+    flex: 1,
+    backgroundColor: theme.colors.white,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+    padding: theme.padding.small,
+  },
 });
 
 const ReviewItem = ({ review }) => {
   // Single review item
-  <View style={styles.container}> 
+  return (
+    <View style={styles.reviewItem}> 
     <Text type='rating'>{review.rating}</Text>
     <Text type='username'>{review.user.username}</Text>
-    <Text type='created'>{review.createdAt}</Text>
+    <Text type='created'>{format(new Date(review.createdAt), 'dd.MM.yyyy')}</Text>
     <Text type='review'>{review.text}</Text>
-  </View>;
+  </View>
+  );
 };
-
 
 
 const SingleRepositoryItem = ({ id }) => {
@@ -32,10 +42,16 @@ const SingleRepositoryItem = ({ id }) => {
     const repository = useRepository(id); // data.repository
 
     if (repository) {
-      return ( repository  &&
+      const { reviews } = repository;
+      console.log(reviews);
 
+      const usedReviews = reviews
+      ? reviews.edges.map(edge => edge.node)
+      : [];
+
+      return ( repository  && 
         <FlatList
-        data={repository.reviews}
+        data={usedReviews }
         renderItem={({ item }) => <ReviewItem review={item} />}
         keyExtractor={({ id }) => id}
         ListHeaderComponent={() => <RepositoryContent repository={repository} git={true}/>}
